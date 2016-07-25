1ba86a7e0c55f9907d46b1b4e5a991041a45e15f
sly <shanglingyue1992@163.com>
2016-07-25 22:44:51 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function() {
-    return gulp.src('sass/demo.scss')
-        .pipe($.sass())
+    return gulp.src('less/index.less')
+        .pipe($.less())
         .pipe($.autoprefixer())
         .pipe(gulp.dest('css'));
+});
+gulp.task('watch',['styles'],function(){
+	gulp.watch('less/index.less', ['styles']);
 });
