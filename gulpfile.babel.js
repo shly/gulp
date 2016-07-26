a6804f7c5ad00edf04265e064ed5649db1ab3a92
shly <1028442429@qq.com>
2016-07-26 17:45:48 +0800

    babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,35 @@
 var gulp = require('gulp');
 var $ =require('gulp-load-plugins')();
-const dirs = {
+var babel = require("gulp-babel");
+const cssdirs = {
     src: 'less',
     dest: 'css'
 };
-const paths = {
-    src: `${dirs.src}/demo.less`,
-    dest: `${dirs.dest}`
+const csspaths = {
+    src: `${cssdirs.src}/demo.less`,
+    dest: `${cssdirs.dest}`
+};
+const jsdirs = {
+    src: 'ES6',
+    dest: 'Javascript'
+};
+const jspaths = {
+    src: `${jsdirs.src}/testES6.js`,
+    dest: `${jsdirs.dest}`
 };
 gulp.task('styles', () => {
-    return gulp.src(paths.src)
+    return gulp.src(csspaths.src)
         .pipe($.less())
         .pipe($.autoprefixer())
-        .pipe(gulp.dest(paths.dest));	
+        .pipe(gulp.dest(csspaths.dest));	
+});
+gulp.task('javascript', () => {
+    return gulp.src(jspaths.src)
+        .pipe(babel())
+        .pipe(gulp.dest(jspaths.dest));	
+});
+gulp.task('watch',()=>{
+    gulp.watch(csspaths.src,'styles');
+    gulp.watch(jspaths.src,'javascript');
+
 });
